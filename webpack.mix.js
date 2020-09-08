d8fabe4e8d54f2294490dedd03502b7b2dea80c7
Norman Huth <norman@huth.it>
2020-09-08 23:21:03 +0200

    Laravel 8.0.0 1st (SCSS instant of CSS)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -7,13 +7,13 @@ const tailwindcss = require('tailwindcss')
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.config.js') ],
-    })
+    });
